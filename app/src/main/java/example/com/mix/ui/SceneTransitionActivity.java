111aa8a4887c39edbae852a67ee04bcf13b87707
gordon <skcalm@yeah.net>
2017-07-06 17:44:07 +0800

    update scene transition

--- app/src/main/java/example/com/mix/ui/SceneTransitionActivity.java
+++ app/src/main/java/example/com/mix/ui/SceneTransitionActivity.java
@@ -3,6 +3,7 @@ package example.com.mix.ui;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.transition.ChangeBounds;
 import android.support.transition.Scene;
@@ -11,7 +12,11 @@ import android.support.transition.TransitionManager;
 import android.support.transition.TransitionSet;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 
 import example.com.mix.R;
@@ -22,6 +27,7 @@ public class SceneTransitionActivity extends BaseActivity {
     /*views*/
     private ViewGroup sceneRoot;
     private View viewScene1;
+    private View viewScene2;
     private Scene sceneStart;
     private Scene sceneEnd;
     private Scene scenePersonalInfo;
@@ -31,6 +37,8 @@ public class SceneTransitionActivity extends BaseActivity {
 
     /*variants*/
     private boolean isCollapsed;
+    private GestureDetector expandBtmDetector;
+    private GestureDetector collapsedBtmDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +49,13 @@ public class SceneTransitionActivity extends BaseActivity {
         initView();
     }
 
-    private void initVal(){
+    private void initVal() {
         sceneRoot = (ViewGroup) findViewById(R.id.scene_root);
         viewScene1 = findViewById(R.id.scene_1);
+        viewScene2 = LayoutInflater.from(this).inflate(R.layout.layout_scene_2, sceneRoot, false);
 
         sceneStart = new Scene(sceneRoot, viewScene1);
-        sceneEnd = Scene.getSceneForLayout(sceneRoot, R.layout.layout_scene_2, this);
+        sceneEnd = new Scene(sceneRoot, viewScene2);
         scenePersonalInfo = Scene.getSceneForLayout(sceneRoot, R.layout.layout_scene_3, this);
 
 
@@ -81,7 +90,7 @@ public class SceneTransitionActivity extends BaseActivity {
 
             @Override
             public void onTransitionEnd(@NonNull Transition transition) {
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
                             SceneTransitionActivity.this,
                             new Pair<View, String>(findViewById(R.id.menu), "avatar"),
@@ -95,7 +104,7 @@ public class SceneTransitionActivity extends BaseActivity {
                             sceneStart.enter();
                         }
                     }, 350);
-                }else{
+                } else {
                     startActivity(new Intent(SceneTransitionActivity.this, PersonalInfoActivity.class));
                 }
             }
@@ -117,12 +126,83 @@ public class SceneTransitionActivity extends BaseActivity {
         });
     }
 
-    private void initView(){
+    private void initView() {
         CommonUtil.setFullScreen(this);
+
+        expandBtmDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if(velocityY > ViewConfiguration.get(SceneTransitionActivity.this).getScaledMinimumFlingVelocity()){
+                    TransitionManager.go(sceneEnd, collapsedTransitionSet);
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                if (e2.getY() - e1.getY() >
+                                ViewConfiguration.get(SceneTransitionActivity.this).getScaledTouchSlop()) {
+                    TransitionManager.go(sceneEnd, collapsedTransitionSet);
+                    final long now = SystemClock.uptimeMillis();
+                    viewScene1.onTouchEvent(MotionEvent.obtain(now, now,
+                            MotionEvent.ACTION_CANCEL, 0.0f, 0.0f, 0));
+                }
+
+                return true;
+            }
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+        });
+
+        collapsedBtmDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if(velocityY < 0 && Math.abs(velocityY) > ViewConfiguration.get(SceneTransitionActivity.this).getScaledMinimumFlingVelocity()){
+                    TransitionManager.go(sceneStart, expandTransitionSet);
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                if (e1.getY() - e2.getY() >
+                                ViewConfiguration.get(SceneTransitionActivity.this).getScaledTouchSlop()) {
+                    TransitionManager.go(sceneStart, expandTransitionSet);
+                    final long now = SystemClock.uptimeMillis();
+                    viewScene2.onTouchEvent(MotionEvent.obtain(now, now,
+                            MotionEvent.ACTION_CANCEL, 0.0f, 0.0f, 0));
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+        });
+
+        viewScene1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return expandBtmDetector.onTouchEvent(event);
+            }
+        });
+
+        viewScene2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return collapsedBtmDetector.onTouchEvent(event);
+            }
+        });
     }
 
-    public void onClick(View v){
-        switch (v.getId()){
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.arrow:
                 TransitionManager.go(isCollapsed ? sceneStart : sceneEnd,
                         isCollapsed ? expandTransitionSet : collapsedTransitionSet);
@@ -132,10 +212,5 @@ public class SceneTransitionActivity extends BaseActivity {
                 TransitionManager.go(scenePersonalInfo, personalInfoTransitionSet);
                 break;
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
     }
 }
